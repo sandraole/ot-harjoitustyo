ba2d8a43f4509c637be1d30a33fad987d1faebd9
Sandra Ole <sandra.ole@hotmail.fi>
2025-04-08 14:52:56 +0300

    Add folder services and file user_service.py

---
+++ src/services/user_service.py
@@ -0,0 +1,28 @@
+import json
+
+class UserService:
+    def __init__(self):
+        self._users = {}
+        self._load_users()
+
+    def _load_users(self):
+        try:
+            with open("data/users.json", "r") as file:
+                self._users = json.load(file)
+        except FileNotFoundError:
+            self._users = {}
+
+    def _save_users(self):
+        with open("data/users.json", "w") as file:
+            json.dump(self._users, file)
+
+    def create_user(self, username, password):
+        if username in self._users:
+            raise Exception("User already exists")
+        self._users[username] = password
+        self._save_users()
+
+    def authenticate(self, username, password):
+        return self._users.get(username) == password
+
+user_service = UserService()
