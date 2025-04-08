a356723abf42492130cf28c09d76c405c96b03c1
Sandra Ole <sandra.ole@hotmail.fi>
2025-04-08 14:51:05 +0300

    Update ui.py

--- src/ui/ui.py
+++ src/ui/ui.py
@@ -1,6 +1,8 @@
 import tkinter as tk
-from tkinter import ttk
-
+from tkinter import ttk, messagebox
+from services.user_service import user_service
+from .register import RegisterView
+from .main import MainView
 
 class UI:
 
@@ -9,42 +11,41 @@ class UI:
         self._frame = None
 
     def start(self):
-        self._show_main_view()
+        self._show_login_view()
 
-    def _show_main_view(self):
+    def _show_login_view(self):
         if self._frame:
             self._frame.destroy()
 
         self._frame = tk.Frame(master=self._root, bg="#3498db", padx=10, pady=10)
         self._frame.pack(fill="both", expand=True)
 
-        heading_label = tk.Label(
-            self._frame, text="Budgeting Application", font=("Arial", 16), bg="#3498db", fg="white"
-        )
-        heading_label.pack(pady=10)
+        tk.Label(self._frame, text="Login", font=("Arial", 16), bg="#3498db", fg="white").pack(pady=10)
 
-        # Username
-        username_label = tk.Label(
-            self._frame, text="Username:", bg="#3498db", fg="white"
-        )
-        username_label.place(relx=0.1, rely=0.2)
-
+        tk.Label(self._frame, text="Username", bg="#3498db", fg="white").pack()
         username_entry = ttk.Entry(self._frame)
-        username_entry.place(relx=0.3, rely=0.2, relwidth=0.6)
+        username_entry.pack()
 
-        # Password
-        password_label = tk.Label(
-            self._frame, text="Password:", bg="#3498db", fg="white"
-        )
-        password_label.place(relx=0.1, rely=0.3)
-
+        tk.Label(self._frame, text="Password", bg="#3498db", fg="white").pack()
         password_entry = ttk.Entry(self._frame, show="*")
-        password_entry.place(relx=0.3, rely=0.3, relwidth=0.6)
+        password_entry.pack()
 
-        # Register button
-        register_button = ttk.Button(self._frame, text="Register")
-        register_button.place(relx=0.3, rely=0.45, relwidth=0.28)
+        ttk.Button(self._frame, text="Sign In", command=lambda: self._sign_in(username_entry.get(), password_entry.get())).pack(pady=5)
+        ttk.Button(self._frame, text="Register", command=self._show_register_view).pack()
 
-        # Sign In button
-        signin_button = ttk.Button(self._frame, text="Sign In")
-        signin_button.place(relx=0.62, rely=0.45, relwidth=0.28)
+    def _sign_in(self, username, password):
+        if user_service.authenticate(username, password):
+            if self._frame:
+                self._frame.destroy()
+
+            self._frame = MainView(self._root)
+            self._frame.pack(fill="both", expand=True)
+        else:
+            messagebox.showerror("Error", "Invalid username or password")
+
+    def _show_register_view(self):
+        if self._frame:
+            self._frame.destroy()
+
+        self._frame = RegisterView(self._root, self.start)
+        self._frame.pack(fill="both", expand=True)
