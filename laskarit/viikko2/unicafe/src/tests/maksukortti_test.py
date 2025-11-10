fad7c2f9f54741342bbd7c9e6f281a69830069ba
Sandra Ole <sandra.ole@hotmail.fi>
2025-11-10 23:06:16 +0200

    More tests

--- laskarit/viikko2/unicafe/src/tests/maksukortti_test.py
+++ laskarit/viikko2/unicafe/src/tests/maksukortti_test.py
@@ -7,3 +7,31 @@ class TestMaksukortti(unittest.TestCase):
 
     def test_luotu_kortti_on_olemassa(self):
         self.assertNotEqual(self.maksukortti, None)
+
+    def test_saldo_alussa_oikein(self):
+        self.assertEqual(self.maksukortti.saldo_euroina(), 10.0)
+
+    def test_rahan_lataaminen_kasvattaa_saldoa_oikein(self):
+        self.maksukortti.lataa_rahaa(2500)
+
+        self.assertEqual(self.maksukortti.saldo_euroina(), 35.0)
+
+    def test_saldo_vahenee_oikein_jos_rahaa_on_tarpeeksi(self):
+        self.maksukortti.ota_rahaa(300)
+    
+        self.assertEqual(self.maksukortti.saldo_euroina(), 7.0)
+
+    def test_saldo_ei_muutu_jos_rahaa_ei_ole_tarpeeksi(self):
+        self.maksukortti.ota_rahaa(3000)
+
+        self.assertEqual(self.maksukortti.saldo_euroina(), 10.0)
+    
+    def test_palauttaa_true_jos_rahat_riittivät(self):
+        result = self.maksukortti.ota_rahaa(250)
+        
+        self.assertEqual(result, True)
+
+    def test_palauttaa_false_jos_rahat_eivät_riittäneet(self):
+        result = self.maksukortti.ota_rahaa(2500)
+
+        self.assertEqual(result, False)
