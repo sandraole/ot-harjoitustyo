b54426e5e6305c4fd1ebd9a90431c45651566a04
Sandra Ole <sandra.ole@hotmail.fi>
2025-12-09 15:13:22 +0200

    separate tests to own files

---
+++ src/tests/book_repository_test.py
@@ -0,0 +1,43 @@
+import unittest
+import os
+from repositories.book_repository import BookRepository
+
+
+class TestBookRepository(unittest.TestCase):
+
+    def setUp(self):
+        self.test_file = "src/data/test_books.json"
+        os.makedirs("src/data", exist_ok=True)
+
+        if os.path.exists(self.test_file):
+            os.remove(self.test_file)
+
+        self.book_repository = BookRepository(self.test_file)
+
+    def tearDown(self):
+        if os.path.exists(self.test_file):
+            os.remove(self.test_file)
+
+    def test_add_book(self):
+        self.book_repository.add_book("Test Book", "Test Author", 123)
+
+        books = self.book_repository.get_all()
+        self.assertEqual(len(books), 1)
+        self.assertEqual(books[0]["title"], "Test Book")
+        self.assertEqual(books[0]["author"], "Test Author")
+        self.assertEqual(books[0]["pages"], 123)
+
+    def test_delete_book(self):
+        self.book_repository.add_book("Book 1", "Author 1", 100)
+        self.book_repository.add_book("Book 2", "Author 2", 200)
+        self.book_repository.add_book("Book 3", "Author 3", 300)
+
+        books_before = self.book_repository.get_all()
+        self.assertEqual(len(books_before), 3)
+        self.book_repository.delete_by_index(1)
+
+        books_after = self.book_repository.get_all()
+        self.assertEqual(len(books_after), 2)
+
+        titles = [book["title"] for book in books_after]
+        self.assertEqual(titles, ["Book 1", "Book 3"])
